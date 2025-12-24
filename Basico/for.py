b18e47014c64e32266c39400e934f7270b20cb45
Lucas Ferreira de Almeida <lucasfalmeida07@gmail.com>
2025-12-24 15:39:52 -0300

    dia 11

--- Basico/for.py
+++ Basico/for.py
@@ -1,3 +1,54 @@
+# Primeiro video (Introdução ao for / in)
+
+texto = 'Python'
+
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
+
+
+# Segundo video (range + for)
+
+'''
+for + range
+range -> range(start,stop,step)
+'''
+
+
+numeros = range(0,10,2)
+
+for numero in numeros:
+    print(numero)
+
+
+# Terceiro video (next, iter, iterável e iterador)
+
+
+"""
+Iteravel -> str,range, etc
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+"""
+# o que isso é por baixo dos panos
+for letra in texto 
+texto = 'Luiz' # iteravel
+iterador = iter(texto) #iterador
+
+while True:
+    try:
+        letra = next(iterador)
+        print(letra)
+    except StopIteration:
+        break
+
+# é a mesma coisa de cima 
+for letra in texto
+    print(letra)
+    
+
 # Quarto video (continue, break, else, etc)
 
 for i in range(10):
